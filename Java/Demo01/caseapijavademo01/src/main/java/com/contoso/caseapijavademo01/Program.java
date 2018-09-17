2d591633cb1fd6ae15c5170db6f485a9f55a50e8
Jui-wen Liu (Infosys Ltd) <v-juili@microsoft.com>
2018-09-17 13:33:15 -0700

    Rename App to Program for harmonization purpose.

---
+++ Java/Demo01/caseapijavademo01/src/main/java/com/contoso/caseapijavademo01/Program.java
@@ -0,0 +1,13 @@
+package com.contoso.caseapijavademo01;
+
+/**
+ * Hello world!
+ *
+ */
+public class Program 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
