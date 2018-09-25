8390e9e8def82033f3d62ce1689baa77c0fab07e
Jui-wen Liu (Infosys Ltd) <v-juili@microsoft.com>
2018-09-25 09:53:49 -0700

    Create Note class implementing INote.

---
+++ Java/Demo02/caseapijavademo02/src/main/java/com/contoso/caseapijavademo02/DO/Note.java
@@ -0,0 +1,18 @@
+package com.contoso.caseapijavademo02.DO;
+
+import com.contoso.caseapijavademo02.DO.Interfaces.INote;
+
+class Note implements INote
+{
+
+    @Override
+    public void importFromJson(String input) {
+
+    }
+
+    @Override
+    public String outputToJson() {
+        return null;
+    }
+
+} 
