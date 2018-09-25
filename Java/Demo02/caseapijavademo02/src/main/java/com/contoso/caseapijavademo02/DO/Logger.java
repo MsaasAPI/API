ad8d3a0e00d3a280dd02e96ce70ef89301cb9583
Jui-wen Liu (Infosys Ltd) <v-juili@microsoft.com>
2018-09-24 18:41:52 -0700

    Implement iLogger and create Logger.

---
+++ Java/Demo02/caseapijavademo02/src/main/java/com/contoso/caseapijavademo02/DO/Logger.java
@@ -0,0 +1,9 @@
+package com.contoso.caseapijavademo02.DO;
+
+class Logger implements iLogger
+{
+    @Override
+    public void log() {
+
+    }
+}
---
+++ Java/Demo02/caseapijavademo02/src/main/java/com/contoso/caseapijavademo02/DO/iLogger.java
@@ -0,0 +1,7 @@
+package com.contoso.caseapijavademo02.DO;
+
+public interface iLogger
+{
+    String filePath = "";
+    void log();
+}
