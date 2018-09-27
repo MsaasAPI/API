1b29de6f9dba8d1f39ab11d1e10cc1e354bf79d9
Jui-wen Liu (Infosys Ltd) <v-juili@microsoft.com>
2018-09-26 19:21:11 -0700

    Add overlooked INote.java.

--- Java/Demo02/caseapijavademo02/src/main/java/com/contoso/caseapijavademo02/DO/Interfaces/iNote.java
+++ Java/Demo02/caseapijavademo02/src/main/java/com/contoso/caseapijavademo02/DO/Interfaces/iNote.java
@@ -2,6 +2,6 @@ package com.contoso.caseapijavademo02.DO.Interfaces;
 
 public interface INote extends IEntityBase
 {
-    void setNote(String noteInput);
-    String getNote();
+    void setContent(String newContent);
+    String getContent();
 }
