9f937de5e98ac2e3b299cfbc7db2b4eada760b8f
Jui-wen Liu (Infosys Ltd) <v-juili@microsoft.com>
2018-09-26 19:19:47 -0700

    Implement ICustomer.

--- Java/Demo02/caseapijavademo02/src/main/java/com/contoso/caseapijavademo02/DO/Interfaces/iCustomer.java
+++ Java/Demo02/caseapijavademo02/src/main/java/com/contoso/caseapijavademo02/DO/Interfaces/iCustomer.java
@@ -1,5 +1,12 @@
 package com.contoso.caseapijavademo02.DO.Interfaces;
 
+import java.util.List;
+
+import com.contoso.caseapijavademo02.DO.Contact;
+
 public interface ICustomer extends IEntityBase
 {
+    void setContacts(List<IContact> contacts);
+    List<IContact> getContacts();
+    List<IContact> findContactsByKeyword(String keyword);
 }
