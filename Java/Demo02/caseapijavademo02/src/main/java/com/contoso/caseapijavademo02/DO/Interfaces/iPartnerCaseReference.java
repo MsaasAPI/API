32dcd8d358fed213fe8054f5810007bf4f5f5c16
Jui-wen Liu (Infosys Ltd) <v-juili@microsoft.com>
2018-09-27 09:46:27 -0700

    Implement PartnerCaseReferences which in turn implements IPartnerCaseReferences.

--- Java/Demo02/caseapijavademo02/src/main/java/com/contoso/caseapijavademo02/DO/Interfaces/iPartnerCaseReference.java
+++ Java/Demo02/caseapijavademo02/src/main/java/com/contoso/caseapijavademo02/DO/Interfaces/iPartnerCaseReference.java
@@ -2,4 +2,14 @@ package com.contoso.caseapijavademo02.DO.Interfaces;
 
 public interface IPartnerCaseReference extends IEntityBase
 {
+    public void setPartnerCaseId(String newPartnerCaseId);
+    public String getPartnerCaseId();
+    public void setPartnerCaseState(String newPartnerCaseState);
+    public String getPartnerCaseState();
+    public void setPartnerAgentInformation(IPartnerAgentInformation newPartnerAgentInformation);
+    public IPartnerAgentInformation getPartnerAgentInformation();
+    public void setPartnerName(String newPartnerName);
+    public String getPartnerName();
+    public void setPartnerId(String newPartnerId);
+    public String getPartnerId();
 }
