64c5c53073535288e8bdc55a9dfe7fe65be73853
Jui-wen Liu (Infosys Ltd) <v-juili@microsoft.com>
2018-09-26 19:16:15 -0700

    Implement Case which in turn implements ICase.

---
+++ Java/Demo02/caseapijavademo02/src/main/java/com/contoso/caseapijavademo02/DO/Case.java
@@ -0,0 +1,104 @@
+package com.contoso.caseapijavademo02.DO;
+
+import java.util.*;
+
+import com.contoso.caseapijavademo02.DO.Interfaces.*;
+import com.google.gson.annotations.*;
+
+public class Case implements ICase
+{
+    @Expose @SerializedName("Customer") private ICustomer customer;
+    @Expose @SerializedName("Notes") private List<INote> notes;
+    @Expose @SerializedName("PartnerCaseReferences") private List<IPartnerCaseReference> partnerCaseReferences;
+
+    public Case() {
+        super();
+    }
+
+    public Case(
+            ICustomer newCustomer, 
+            List<INote> newNotes, 
+            List<IPartnerCaseReference> newPartnerCaseReferences) {
+        super();
+        customer = newCustomer;
+        notes = newNotes;
+        partnerCaseReferences = newPartnerCaseReferences;
+    }
+
+    @Override
+    public void importFromJson(String input) {
+
+    }
+
+    @Override
+    public String outputToJson() {
+        return null;
+    }
+
+    @Override
+    public boolean isUnpopulated() {
+        if (!customer.isUnpopulated())
+            return false;
+
+        for(INote n : notes){
+            if(!n.isUnpopulated())
+                return false;
+        }
+
+        for(IPartnerCaseReference p : partnerCaseReferences){
+            if(!p.isUnpopulated())
+                return false;
+        }
+
+        return true;
+    }
+
+    @Override
+    public void setCustomer(ICustomer newCustomer) {
+        customer = newCustomer;
+    }
+
+    @Override
+    public ICustomer getCustomer() {
+        return customer;
+    }
+
+    @Override
+    public void setNotes(List<INote> newNotes) {
+        notes = newNotes;
+    }
+
+    @Override
+    public List<INote> getNotes() {
+        return notes;
+    }
+    
+    @Override
+    public List<INote> findNotesByKeyword(String keyword) {
+        List<INote> matchingNotes = new ArrayList<INote>();
+        String cappedKeyword = keyword.toUpperCase();
+
+        // Return all notes containing keyword in major attributes. Case insensitive.
+        for(INote n : notes){
+            if (n.getContent().toUpperCase().contains(cappedKeyword))
+                matchingNotes.add(n);
+        }
+
+        return matchingNotes;
+    }
+
+    @Override
+    public void setPartnerCaseReferences(List<IPartnerCaseReference> newPartnerCaseReferences) {
+        partnerCaseReferences = newPartnerCaseReferences;
+    }
+
+    @Override
+    public List<IPartnerCaseReference> getPartnerCaseReferences() {
+        return partnerCaseReferences;
+    }
+
+    @Override
+    public List<IPartnerCaseReference> findPartnerCaseReferencesByKeyword(String keyword) {
+        return null;
+    }
+}
--- Java/Demo02/caseapijavademo02/src/main/java/com/contoso/caseapijavademo02/DO/Interfaces/iCase.java
+++ Java/Demo02/caseapijavademo02/src/main/java/com/contoso/caseapijavademo02/DO/Interfaces/iCase.java
@@ -1,5 +1,15 @@
 package com.contoso.caseapijavademo02.DO.Interfaces;
 
+import java.util.List;
+
 public interface ICase extends IEntityBase
 {
+    void setCustomer(ICustomer newCustomer);
+    ICustomer getCustomer();
+    void setNotes(List<INote> newNotes);
+    List<INote> getNotes();
+    List<INote> findNotesByKeyword(String keyword);
+    void setPartnerCaseReferences(List<IPartnerCaseReference> newPartnerCaseReferences);
+    List<IPartnerCaseReference> getPartnerCaseReferences();
+    List<IPartnerCaseReference> findPartnerCaseReferencesByKeyword(String keyword);
 }
