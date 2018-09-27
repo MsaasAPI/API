2549a98a453e4f40d82b3b27df8d7c45610c016f
Jui-wen Liu (Infosys Ltd) <v-juili@microsoft.com>
2018-09-27 09:48:12 -0700

    Add all other misc updates during the build and also the Program (console) changes.

--- Java/Demo02/caseapijavademo02/src/main/java/com/contoso/caseapijavademo02/Program.java
+++ Java/Demo02/caseapijavademo02/src/main/java/com/contoso/caseapijavademo02/Program.java
@@ -1,13 +1,8 @@
 package com.contoso.caseapijavademo02;
 
-/**
- * Hello world!
- *
- */
 public class Program 
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
     }
 }
