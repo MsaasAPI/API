2fbbd16f2133087f43b955bd8d025b1b9587d367
Jui-wen Liu (Infosys Ltd) <v-juili@microsoft.com>
2018-10-09 08:02:23 -0700

    Add DL layer with IRepositoryAccessor as placeholder.

---
+++ Java/Demo02/caseapijavademo02/src/main/java/com/contoso/caseapijavademo02/DL/Interfaces/IRepositoryAccessor.java
@@ -0,0 +1,8 @@
+package com.contoso.caseapijavademo02.DL.Interfaces;
+
+/**
+ * Serves as base for all objects making create, read, update and delete (CRUD) operations on repositories.
+ */
+interface IRepositoryAccessor
+{
+}
