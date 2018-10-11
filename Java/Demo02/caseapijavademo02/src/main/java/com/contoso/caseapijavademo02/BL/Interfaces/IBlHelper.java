db7fe7ec13923a9113faec4e1ee812c54f803be2
Jui-wen Liu (Infosys Ltd) <v-juili@microsoft.com>
2018-10-11 11:50:01 -0700

    Add methodsd into IBlHelper.
    Methods:
    String getAccessToken();
    String getLogName();
    String parseResponseBody(HttpResponse response);

--- Java/Demo02/caseapijavademo02/src/main/java/com/contoso/caseapijavademo02/BL/Interfaces/IBlHelper.java
+++ Java/Demo02/caseapijavademo02/src/main/java/com/contoso/caseapijavademo02/BL/Interfaces/IBlHelper.java
@@ -1,8 +1,13 @@
 package com.contoso.caseapijavademo02.BL.Interfaces;
 
+import org.apache.http.HttpResponse;
+
 /**
  * Contract for general-purpose Business Layer helper object.
  */
 public interface IBlHelper
 {
+    String getAccessToken();
+    String getLogName();
+    String parseResponseBody(HttpResponse response);
 }
