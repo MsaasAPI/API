1a00ff1c443068e0e156888f7a6265285935f1c3
Jui-wen Liu (Infosys Ltd) <v-juili@microsoft.com>
2018-10-12 15:58:00 -0700

    Drop 3 interfaces to start over the BL design phase.
    IBlHelper
    IScenarioConfigurator
    IScenarioRunner

--- Java/Demo02/caseapijavademo02/src/main/java/com/contoso/caseapijavademo02/BL/Interfaces/IScenarioRunner.java
+++
@@ -1,9 +0,0 @@
-package com.contoso.caseapijavademo02.BL.Interfaces;
-
-public interface IScenarioRunner
-{
-    void provisionRequest();
-    void sendRequest();
-    void cacheTransaction();
-    void logTransaction();
-}
